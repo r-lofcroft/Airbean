0bf3a4a80607bd9c3fcf49cf6bc9608751152ccc
Robin Williscroft <robin.williscroft@gmail.com>
2021-04-30 14:26:10 +0200

    Integrate KhalidRe accounts.js to server.js

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const lowdb = require("lowdb");
 const { nanoid } = require("nanoid");
 const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync("menu.json");
-const database = lowdb(adapter);
+const menuAdapter = new FileSync("menu.json");
+const menuDatabase = lowdb(menuAdapter);
+
+const accountAdapter= new FileSync("accounts.json");
+const accountDatabase = lowdb(accountAdapter);
 
 const app = express();
 app.use(express.json());
@@ -16,7 +19,7 @@ let orders = [];
 
 /*Hämtar menu.JSON*/
 app.get("/api/coffee", (request, response) => {
-  response.json(database);
+  response.json(menuDatabase);
 });
 
 app.get('/api/order', (request, response) => {
@@ -26,16 +29,64 @@ app.get('/api/order', (request, response) => {
 /*Lägger till order*/
 app.post('/api/order', (request, response) => {
   const orderItem = request.body;
-  orderItem.id = nanoid(5); 
-  orderItem.eta =  moment().format("ddd, hA" ); 
-  console.log('Ditt orderID är:', orderItem.id);
-  console.log('ETA:', orderItem.eta);
-  orders.push(orderItem);
+  orderItem.orderID = nanoid(5); 
+  function randomInt(min, max){
+    return Math.random() * (min , max)+min;
+  }
+  let Idag = new Date();
+  let date = new Date();
+  date.setDate(date.getDate() + randomInt(1, 28));
+  orderItem.eta =   moment(date).format('dddd, MMMM Do YYYY, h a')
+  orderItem.date =   moment().format('dddd, MMMM Do YYYY, h a')
 
+  console.log('Ditt orderID är:', orderItem.orderid);
+  console.log('Ordern var lagd:', orderItem.date);
+  console.log('ETA:', orderItem.eta);
+
+  orders.push(orderItem);
   //Vad ska vi skicka tillbaka för svar?
   response.json(orderItem);
 });
 
+
+/* Se alla konton */
+app.get("/api/account", (request, response) => {
+  response.json(accountDatabase);
+});
+
+/*Skapa nya konton*/ 
+app.post("/api/account", (request, response) => {
+  const account = request.body;
+  console.log("konto att lägga till:", account);
+
+  const usernameInUse = accountDatabase
+    .get("accounts")
+    .find({ username: account.username })
+    .value();
+  const emailInUse = accountDatabase
+    .get("accounts")
+    .find({ email: account.email })
+    .value();
+  console.log("Email Används", emailInUse);
+  console.log("Användarnamn Används:", usernameInUse);
+
+  const result = {
+    allowed: false,
+    usernameInUse: false,
+    emailInUse: false,
+  };
+  if (usernameInUse) {
+    result.usernameInUse = true;
+  }
+  if (emailInUse) {
+    result.emailInUse = true;
+  }
+  if (!result.usernameInUse && !result.emailInUse) {
+    accountDatabase.get("accounts").push(account).write();
+    result.allowed = true;
+  }
+  response.json(result);
+});
 
 app.listen(8001, () => {
   console.log("Server started");
