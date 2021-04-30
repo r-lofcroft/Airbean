b932e0d42610fe0deb416469ab4e49fdabdc12c1
KhalidRe <khaledtumah@gmail.com>
2021-04-30 10:22:31 +0200

    entry

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const lowdb = require('lowdb');
-const { nanoid } = require('nanoid');
-const FileSync = require('lowdb/adapters/FileSync');
+const express = require("express");
+const lowdb = require("lowdb");
+const { nanoid } = require("nanoid");
+const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync('menu.json');
+const adapter = new FileSync("menu.json");
 const database = lowdb(adapter);
 
 const app = express();
@@ -11,10 +11,10 @@ const app = express();
 app.use(express.json());
 
 /*Hämtar menu.JSON*/
-app.get('/api/coffee', (request, response) => {
+app.get("/api/coffee", (request, response) => {
   response.json(database);
 });
 
 app.listen(8001, () => {
-  console.log('Server started');
+  console.log("Server started");
 });
